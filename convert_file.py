14bf9b446f971539fd304e0726b95d93661507fd
Jessieguojunyi <jessieguo.guo@gmail.com>
2021-05-10 12:17:55 -0400

    Update helper function

--- convert_file.py
+++ convert_file.py
@@ -1,3 +1,7 @@
+# We follow the instructions provided by Amany in her blog:
+# https://medium.com/swlh/preprocessing-criteo-dataset-for-prediction-of-click-through-rate-on-ads-7dee096a2dd9
+# This file provide several healp function that can be utilzed during data reading and save processes
+
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, BooleanType, FloatType
@@ -17,16 +21,16 @@ print('Session created')
 def read_parquet(path):
     df = (spark.read
     .option("delimiter", "\t")
-    .parquet(path)   # Creates a DataFrame from Parquet after reading in the file
+    .parquet(path)
     )
     return df
 
 def write_parquet(df, path):
-    (df.coalesce(1).write                       # Our DataFrameWriter
+    (df.coalesce(1).write 
     .option("delimiter", "\t")  
     .option("compression", "snappy")
-    .mode("overwrite")                       # Replace existing files
-    .parquet(path)               # Write DataFrame to parquet files
+    .mode("overwrite")
+    .parquet(path)
     )
 
 def convert_to_numpy(df):
@@ -42,4 +46,11 @@ criteoPath = "/home/ubuntu/criteoSubset_train.parquet/part-00000-b0ec07ee-7d0a-4
 criteo_DF = (spark.read              
   .option("delimiter", "\t")  
   .parquet(criteoPath)   # Creates a DataFrame from Parquet after reading in the file
+)
+
+(criteoDF.write                       # Our DataFrameWriter
+.option("delimiter", "\t")  
+.option("compression", "snappy")
+.mode("overwrite")                       # Replace existing files
+.parquet(path)               # Write DataFrame to parquet files
 )
